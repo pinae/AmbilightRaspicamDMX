c7b3faa259d9d49374516a464c6d8d1ff817bc28
pinae <jonny@pinae.net>
2016-04-07 15:09:54 +0200

    streaming code example

--- ambilight.py
+++ ambilight.py
@@ -1,16 +1,81 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+import io
 import time
+import threading
 import picamera
-import picamera.array
+from PIL import Image
+
+# Create a pool of image processors
+done = False
+lock = threading.Lock()
+pool = []
 
 
+class ImageProcessor(threading.Thread):
+    def __init__(self):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
+
+    def run(self):
+        # This method runs in a separate thread
+        global done
+        while not self.terminated:
+            # Wait for an image to be written to the stream
+            if self.event.wait(1):
+                try:
+                    self.stream.seek(0)
+                    # Read the image and do some processing on it
+                    image = Image.open(self.stream).convert('RGB')
+                    r, g, b = image.getpixel((180, 10))
+                    print("(" + str(r) + "," + str(g) + "," + str(b) + ")")
+                    # Set done to True if you want the script to terminate
+                    # at some point
+                    #done=True
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
+
+
+def streams():
+    while not done:
+        with lock:
+            if pool:
+                processor = pool.pop()
+            else:
+                processor = None
+        if processor:
+            yield processor.stream
+            processor.event.set()
+        else:
+            # When the pool is starved, wait a while for it to refill
+            time.sleep(0.1)
+
+
+# Shut down the processors in an orderly fashion
+def shutdown():
+    while pool:
+        with lock:
+            processor = pool.pop()
+        processor.terminated = True
+        processor.join()
 
 with picamera.PiCamera() as camera:
-    while True:
-        with picamera.array.PiRGBArray(camera) as stream:
-            camera.resolution = (1024, 768)
-            camera.capture(stream, 'rgb')
-            print("Red:   " + str(stream.array[10, 180, 0]))
-            print("Green: " + str(stream.array[10, 180, 1]))
-            print("Blue:  " + str(stream.array[10, 180, 2]))
-            time.sleep(0.02)
+    pool = [ImageProcessor() for i in range(4)]
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture_sequence(streams(), use_video_port=True)
+
+shutdown()
