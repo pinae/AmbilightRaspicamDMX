061b4748ed0c6fe29e48b9cb1a27fb7cff182dc3
pinae <jonny@pinae.net>
2016-04-11 13:23:49 +0200

    more modulo

--- controller.py
+++ controller.py
@@ -59,7 +59,7 @@ def main_loop():
             lower = trunc(state) % len(modes[mode])
             upper = ceil(state) % len(modes[mode])
             if not upper > lower:
-                upper += 1
+                upper = (upper+1) % len(modes[mode])
             phase = state - trunc(state)
             print(str(lower)+":"+str(upper)+": "+str(phase))
             color = (modes[mode][lower][0] * (1 - phase) +
@@ -69,7 +69,7 @@ def main_loop():
                      modes[mode][lower][2] * (1 - phase) +
                      modes[mode][upper][2] * phase)
             dmx_bus.set_channels({1: round(color[0]), 2: round(color[1]), 3: round(color[2])})
-            state += 0.01
+            state += 0.05
             print(str(mode) + ": " + str(state) + " ... color: " + str(color))
         time.sleep(0.05)
 
