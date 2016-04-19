aef17e33c805cccb0a001b7a3c3f7ee088ed148b
pinae <jme@ct.de>
2016-04-19 15:24:56 +0000

    Ola wrapper class

--- AmbilightRaspicamDMX/olawrapper.cpp
+++ AmbilightRaspicamDMX/olawrapper.cpp
@@ -9,30 +9,33 @@ using std::cout;
 using std::endl;
 
 OlaWrapper::OlaWrapper()
+  // Create a new client.
+  :ola_client((ola::client::StreamingClient::Options()))
 {
-    /*unsigned int universe = 0; // universe to use for sending data
-      // turn on OLA logging
-      ola::InitLogging(ola::OLA_LOG_WARN, ola::OLA_LOG_STDERR);
-      ola::DmxBuffer buffer; // A DmxBuffer to hold the data.
-      buffer.Blackout(); // Set all channels to 0
-      // Create a new client.
-      ola::client::StreamingClient ola_client(
-        (ola::client::StreamingClient::Options()));
-      // Setup the client, this connects to the server
-      if (!ola_client.Setup()) {
-        std::cerr << "Setup failed" << endl;
-        exit(1);
-      }
-      // Send 100 frames to the server. Increment slot (channel) 0 each time a
-      // frame is sent.
-      for (unsigned int i = 0; i < 255; i++) {
-        buffer.SetChannel(1, 254-i);
-        if (!ola_client.SendDmx(universe, buffer)) {
-          cout << "Send DMX failed" << endl;
-          exit(1);
-        }
-        usleep(25000); // sleep for 25ms between frames.
-      }
-     */
+  universe = 0; // universe to use for sending data
+  ola::InitLogging(ola::OLA_LOG_WARN, ola::OLA_LOG_STDERR);
+  buffer.Blackout(); // Set all channels to 0
+  // Setup the client, this connects to the server
+  if (!ola_client.Setup()) {
+    std::cerr << "Setup failed" << endl;
+    exit(1);
+  }
 }
 
+void OlaWrapper::setChannel(unsigned int channel, unsigned int value)
+{
+  buffer.SetChannel(channel, value);
+}
+
+void OlaWrapper::blackout()
+{
+  buffer.Blackout();
+}
+
+void OlaWrapper::send()
+{
+  if (!ola_client.SendDmx(universe, buffer)) {
+    cout << "Send DMX failed" << endl;
+    exit(1);
+  }
+}
--- AmbilightRaspicamDMX/olawrapper.h
+++ AmbilightRaspicamDMX/olawrapper.h
@@ -1,11 +1,21 @@
 #ifndef OLAWRAPPER_H
 #define OLAWRAPPER_H
+#include <ola/DmxBuffer.h>
+#include <ola/client/StreamingClient.h>
 
 
 class OlaWrapper
 {
 public:
     OlaWrapper();
+    void setChannel(unsigned int channel, unsigned int value);
+    void blackout();
+    void send();
+
+private:
+    unsigned int universe;
+    ola::DmxBuffer buffer; // A DmxBuffer to hold the data.
+    ola::client::StreamingClient ola_client;
 };
 
 #endif // OLAWRAPPER_H
